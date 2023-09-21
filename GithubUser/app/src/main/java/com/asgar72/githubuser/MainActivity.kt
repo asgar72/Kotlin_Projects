d9515c7ef98db0f41c25530b4cf316e9227af04f
asgar72 <72asgar72@gmail.com>
2023-09-22 02:35:01 +0530

    desgin completed

--- GithubUser/app/src/main/java/com/asgar72/githubuser/MainActivity.kt
+++ GithubUser/app/src/main/java/com/asgar72/githubuser/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.asgar72.githubuser
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asgar72.githubuser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         supportActionBar?.hide()
+
+        binding.btnsearch.setOnClickListener {
+            val intent = Intent(this, UserData::class.java)
+            startActivity(intent)
+        }
     }
 }
