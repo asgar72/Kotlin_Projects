9d3a98d6d75e388676895dd79d94072a92361509
asgar72 <72asgar72@gmail.com>
2023-10-26 12:00:20 +0530

    bottom nav des

--- ToDoTimer/app/src/main/java/com/asgar72/todotimer/MainActivity.kt
+++ ToDoTimer/app/src/main/java/com/asgar72/todotimer/MainActivity.kt
@@ -2,7 +2,6 @@ package com.asgar72.todotimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -12,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
-        val bottomView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val bottomView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
         bottomView.setOnItemSelectedListener {
 
@@ -31,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     private  fun replaceWithFragment(fragment: Fragment){
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.commit()
     }
 }
